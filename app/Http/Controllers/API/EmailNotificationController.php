836d3c440e61453202bdfd45a687ab47d9cc7744
evanafridianto <evankota11@gmail.com>
2023-01-31 18:37:13 +0700

    update api notify

--- app/Http/Controllers/API/EmailNotificationController.php
+++ app/Http/Controllers/API/EmailNotificationController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Notifications\SmarkApi;
-use Illuminate\Notifications\Notification;
+use Notification;
 
 class EmailNotificationController extends Controller
 {
